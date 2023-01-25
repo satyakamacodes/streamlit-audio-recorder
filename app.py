deb53204149547fa3928c9920d42e92952c11eab
dipanja <worldofbappa@gmail.com>
2023-01-26 01:02:16 +0530

    added audio recorder folder and saving audio

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 """This streamlit app will record audio."""
 
+import datetime
+
 import streamlit as st
 from audio_recorder_streamlit import audio_recorder
 
@@ -7,3 +9,8 @@ audio_bytes = audio_recorder(text="record audio here")
 
 if audio_bytes:
     st.audio(data=audio_bytes, format="audio/wav")
+
+    if st.button("save"):
+        FILENAME = "recordings/recording_" + str(datetime.datetime.now()) + ".wav"
+        with open(FILENAME, "wb") as file:
+            file.write(audio_bytes)
