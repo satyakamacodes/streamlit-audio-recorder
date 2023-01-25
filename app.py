63f00b6ee841cfd79f347144739735fd4dd59596
Bappa <worldofbappa@gmail.com>
2023-01-26 01:13:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import datetime
 import streamlit as st
 from audio_recorder_streamlit import audio_recorder
 
-audio_bytes = audio_recorder(text="record audio here")
+audio_bytes = audio_recorder(text="record audio here", energy_threshold=(-1.0, 1.0),
+  pause_threshold=10.0)
 
 if audio_bytes:
     st.audio(data=audio_bytes, format="audio/wav")
